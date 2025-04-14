c648257a2e165e2cdf673fee08ea5c19fba7d848
Jonas Biener <46632213+JonasBiener@users.noreply.github.com>
2025-04-14 20:32:55 +0200

    Fixed padding in clock-display

--- Lab4_Interrupts.X/clock.c
+++ Lab4_Interrupts.X/clock.c
@@ -17,9 +17,9 @@ void clock_init(clock_t clock){
 }
 
 void print_clock(clock_t const clock){
-    GLCD_Value2Out(0, 2, clock->minutes);
-    GLCD_Value2Out(0, 5, clock->seconds);
-    GLCD_Value2Out(0, 8, clock->milliseconds);
+    GLCD_Value2Out_00(0, 2, clock->minutes, 2);
+    GLCD_Value2Out_00(0, 5, clock->seconds, 2);
+    GLCD_Value2Out_00(0, 8, clock->milliseconds, 2);
 }
 
 void add_ms_to_watch(clock_t const clock, unsigned int milliseconds){
