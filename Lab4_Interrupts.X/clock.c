bcb4e6f789b365bebcc2b9bf83a7adbf5f782a4f
Sebastian Moosbauer <sebastian.moosbauer@thu.de>
2025-04-14 14:26:44 +0200

    add functionality to plot clock to display

--- Lab4_Interrupts.X/clock.c
+++ Lab4_Interrupts.X/clock.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   clock.c
  * Author: Sebastian Moosbauer
  *
@@ -7,11 +7,19 @@
 
 #include <clock.h>
 
+#include "LCD/GLCD_library.h"
+
 void clock_init(clock_t clock){
     clock->hours = 0;
     clock->minutes = 0;
     clock->seconds = 0;
     clock->milliseconds = 0;
+}
+
+void print_clock(clock_t const clock){
+    GLCD_Value2Out(0, 2, clock->minutes);
+    GLCD_Value2Out(0, 5, clock->seconds);
+    GLCD_Value2Out(0, 8, clock->milliseconds);
 }
 
 void add_ms_to_watch(clock_t const clock, unsigned int milliseconds){
@@ -30,6 +38,8 @@ void add_ms_to_watch(clock_t const clock, unsigned int milliseconds){
         clock->minutes -= 60;
         ++clock->hours;
     }
+
+    print_clock(clock);
 }
 
 void add_sec_to_watch(clock_t const clock, unsigned int seconds){
@@ -43,4 +53,6 @@ void add_sec_to_watch(clock_t const clock, unsigned int seconds){
         clock->minutes -= 60;
         ++clock->hours;
     }
+
+    print_clock(clock);
 }
